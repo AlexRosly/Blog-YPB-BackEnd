30e7a2094d946023f49a75787dd1a8e9ca792b3a
AlexRosly <rosly.a777@gmail.com>
2023-12-09 22:59:55 +0200

    change rout

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/post", postRouter);
+app.use("/blog/api/post", postRouter);
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
