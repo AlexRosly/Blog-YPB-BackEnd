317d67cff3cf4398d86afc5d2b39caae6f91fa3a
AlexRosly <rosly.a777@gmail.com>
2023-12-09 23:33:28 +0200

    remove route for swagger

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.json());
 
 app.use("/blog/api/post", postRouter);
 
-// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
